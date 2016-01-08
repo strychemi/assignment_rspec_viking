e342b63f288cb77cca5b4460db0b7732ad845980
Andrew Baik <and.baik@gmail.com>
2016-01-08 13:58:51 -0500

    started bow specs

--- spec/bow_spec.rb
+++ spec/bow_spec.rb
@@ -1,1 +1,17 @@
-# Your code here
+require 'weapons/bow'
+
+describe Bow do
+
+  let(:bow) { Bow.new }
+
+  describe '#initialize' do
+    it 'a bow\'s arrow count is readable' do
+      expect(bow.arrows).to eq(10)
+    end
+
+    it 'bow created with arrow has that many arrows' do
+      bow = Bow.new(99)
+      expect(bow.arrows).to eq(99)
+    end
+  end
+end
